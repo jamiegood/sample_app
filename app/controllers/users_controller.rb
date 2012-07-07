a5456037df5a35214383737cc1508f0a3a4a35b5
Jamie Good <jamie.good@gmail.com>
2012-07-07 16:21:36 +0100

    chapter 7 sign up wip

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,18 @@
 class UsersController < ApplicationController
-  def new
+  def show
+    @user = User.find(params[:id])
   end
+  
+  def new
+    @user = User.new
+  end
+  
+  def create
+    @user = User.create(params[:user])
+    if @user.save
+      #
+    else
+      render :new    
+    end
+  end 
 end
