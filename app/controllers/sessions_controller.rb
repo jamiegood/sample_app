6b2a99afd6727cb6a243faa145bea3f149294112
Jamie Good <jamie.good@gmail.com>
2012-08-05 20:34:53 +0100

    chapter 8

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,6 +5,13 @@ class SessionsController < ApplicationController
   end
   
   def create
+    user = User.find_by_email(params[:session][:email])
+    if user && user.authenicate(params[:session][:password])
+      # successful sign up
+    else
+      flash[:error] = 'Invalid email/password combination'
+      render 'new'
+    end
     
   end
   
