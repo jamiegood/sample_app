d17a03c51827bb827dfd8d18c056f311b7516432
Jamie Good <jamie.good@gmail.com>
2012-08-15 00:30:07 +0100

    chpr 9.13

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -58,8 +58,11 @@ describe "User pages" do
   
   describe "edit" do
     let(:user) { FactoryGirl.create(:user) }
-    before { visit edit_user_path(user) }
-    
+    before do
+      sign_in user
+      visit edit_user_path(user)
+    end
+        
     describe "page" do
       it { should have_selector('h1', text: "Update your profile") }
       it { should have_selector('title', text: "Edit user") }
@@ -67,8 +70,21 @@ describe "User pages" do
     end
     
     describe "with invalid information" do
-      before { click_button "Save Changes" }
-      it { should have_content('error') }
+      let(:new_name)  { "New Name" }
+      let(:new_email) { "new@example.com" }
+      before do
+        fill_in "Name",             with: new_name
+        fill_in "Email",            with: new_email
+        fill_in "Password",         with: user.password
+        fill_in "Confirm Password", with: user.password
+        click_button "Save Changes"
+      end
+
+      it { should have_selector('title', text: new_name) }
+      it { should have_selector('div.alert.alert-success') }
+      it { should have_link('Sign out', href: signout_path) }
+      specify { user.reload.name.should  == new_name }
+      specify { user.reload.email.should == new_email }
     end
     
   end
