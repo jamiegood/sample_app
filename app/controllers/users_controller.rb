e2c6e3b13f6a16cb3a28dd77385e6066c8cfe3e0
Jamie Good <jamie.good@gmail.com>
2012-10-08 14:21:43 +0100

    Finish user edit, update, index and destroy actions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,7 @@ class UsersController < ApplicationController
   
   before_filter :signed_in_user, only: [:edit, :update, :index]
   before_filter :correct_user,   only: [:edit, :update]
+  before_filter :admin_user, only: :destroy
   
   def index
     @users = User.paginate(page: params[:page])
@@ -40,6 +41,14 @@ class UsersController < ApplicationController
     end
   end 
   
+  def destroy
+    User.find(params[:id]).destroy
+    
+    flash[:success] = "User deleted"
+    redirect_to users_url
+    
+  end
+  
   private
 
     def signed_in_user
@@ -53,4 +62,9 @@ class UsersController < ApplicationController
       @user = User.find(params[:id])
       redirect_to(root_path) unless current_user?(@user)
     end
+    
+    def admin_user
+      redirect_to(root_path) unless current_user.admin?
+    end
+    
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -32,7 +32,15 @@ describe User do
   it { should be_valid }
   it { should_not be_admin}
   
-  # describe "with admin attributes set to 'true'"
+  describe "with admin attributes set to 'true'" do
+    before do
+      @user.save!
+      @user.toggle!(:admin)
+    end
+    
+    it { should be_admin }
+  end
+  
   
   describe "when name is not present" do
     before { @user.name = " "}
