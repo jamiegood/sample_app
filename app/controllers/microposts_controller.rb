ee73a5b5821e50c57d8600a19c7296324d0a61e2
Jamie Good <jamie.good@gmail.com>
2012-12-01 13:32:59 +0000

    onto chapter 10.3.3

---
+++ app/controllers/microposts_controller.rb
@@ -0,0 +1,21 @@
+class MicropostsController < ApplicationController
+  before_filter :signed_in_user, only: [:create,  :destroy]
+  
+  def index
+  end
+  
+  def create
+    @micropost = current_user.microposts.build(params[:micropost])
+    if @micropost.save
+      flash[:success] = "Micropost created!"
+      redirect_to root_url
+    else
+      render 'static_pages/home'
+    end
+    
+  end
+  
+  def destroy
+  end
+  
+end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -52,13 +52,6 @@ class UsersController < ApplicationController
   end
   
   private
-
-    def signed_in_user
-      unless signed_in?
-        store_location
-        redirect_to signin_url, notice: "Please sign in."
-      end
-    end  
     
     def correct_user
       @user = User.find(params[:id])
