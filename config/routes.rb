8807c45e5ec11b3b843490c62bc62ead2e4538d9
Jamie Good <jgood@paddpower.com>
2012-12-31 15:02:30 +0000

    chapter 11.2.2

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 SampleApp::Application.routes.draw do
-  resources :users
+  resources :users do
+    member do
+      get :following, :followers
+    end
+  end
   resources :sessions, only: [:new, :create, :destroy]
   resources :microposts, only: [:create, :destroy]
-  
+  resources :relationships, only: [:create, :destory]
   
   root :to => 'static_pages#home'
   match '/help',    to: 'static_pages#help'
