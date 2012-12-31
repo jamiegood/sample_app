08642eab99044e06fa2faf5e30f737060719001f
Jamie Good <jgood@paddpower.com>
2012-12-31 13:16:04 +0000

    chapter 11.14 complete

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -31,6 +31,10 @@ describe User do
   it { should respond_to(:microposts) }
   it { should respond_to(:feed) }
   it { should respond_to(:relationships) }
+  it { should respond_to(:followed_users) }
+  it { should respond_to(:following?) }
+  it { should respond_to(:follow!) }
+  it { should respond_to(:unfollow!) }
   
   it { should be_valid }
   it { should_not be_admin}
@@ -165,5 +169,24 @@ describe User do
       its(:feed) { should_not include(unfollowed_post) }
     end    
   end
+
+  describe "following do"
+    let(:other_user)  { FactoryGirl.create(:user)}
+    before do
+      @user.save
+      @user.follow!(other_user)
+    end
+
+    it { should be_following(other_user)}
+    its(:following_users) { should include(other_user)}
+
+    describe "and unfollowing" do
+      before { @user.unfollow!(other_user) }
+      it { should_not be_following(other_user) }
+      its(:following_user) { should_not include(other_user) }
+    end
+    
+  end
+
   
 end
